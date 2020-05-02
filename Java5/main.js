a4928ad45490debd1f3a7abe217fd47afa687f5b
TaulantiUbt <talifazliu@gmail.com>
2020-05-02 14:29:33 +0200

    detyra me javascript

---
+++ Java5/main.js
@@ -0,0 +1,12 @@
+var x = document.getElementById("logini");
+        var y = document.getElementById("registeri");
+        
+
+        function register() {
+            x.style.left = "-550px";
+            y.style.left = "-650px";
+        };
+        function login() {
+            x.style.left = "10px";
+            y.style.left = "410px";
+        };
