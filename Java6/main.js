a9a2a6f297f9af222ecf149b702ce029453e3754
TaulantiUbt <talifazliu@gmail.com>
2020-05-08 23:33:27 +0200

    detyra perfundimtare java6

--- Java6/main.js
+++ Java6/main.js
@@ -1,12 +1,37 @@
-function modifyDOM(){
-    var createTitle=document.createElement('h1');
-    var nodeText=document.createTextNode('Ky eshte nje titull');
-    createTitle.appendChild(nodeText);
-    document.getElementsByTagName('body')[0].appendChild(createTitle);
+function shtoLajmin() {
+    var titulli = document.createElement("h3");
+    var teksti = document.createElement("p");
+    var titulliContent = document.getElementById("titulli-new").value="";
+    var tekstiContent = document.getElementById("permbajtja-new").value="";
+    if (titulliContent == "" || tekstiContent == ""){
+    alert ("Lajmi duhet te permbaje edhe titull edhe tekst");
+    return;
+    }
+titulli.append(titulliContent);
+teksti.append(tekstiContent);
+var foto= document.createElement("img");
+foto.src ="logo.png";
+foto.setAttribute("class","foto-lajmi");
+var lajemet = document.getElementsByClassName("form-group");
+    if (lajemet.length == 4){
+    lajmet[3].parentNode.removeChild(lajmet[3]);
+    }
+var lajmi = document.createElement("div");
+lajmi.append(foto);
+lajmi.append(titulli);
+lajmi.append(teksti);
+lajmi.setAttribute("class","form-group")
 
-    var titulli=documnet.getElementsByTagName('h1')[0];
-    titulli.style.color="red";
-    titulli.style.display='none';
+var data = document.createElement("h6");
+var data = new Date ();
+var koha = date.getFullYear()+"/"+(date.getMonth()+1)+"/"+date.getDate();
+data.append(koha);
+lajmi.append(data);
+var lajmetDiv = document.getElementsByClassName("right-side");
+lajmetDiv[0].prepend(lajmi);
 
-    titulli.classList.add(titulli);
 }
+    function clearfields(){
+        document.getElementsById("titulli-new").value="";
+        document.getElementsById("permbajtja-new").value="";
+    }
